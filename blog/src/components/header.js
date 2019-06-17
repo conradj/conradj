8837a99817956a6b16ec89dd54c770f330edd5bc
conradj <conradsjackson@gmail.com>
2019-06-17 23:22:14 +0100

    bleurgh

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
-import Navigation from "src/components/Navigation"
+import Navigation from "src/components/navigation"
 
 const StyledHeader = styled.header`
   marginbottom: 1.45rem;
