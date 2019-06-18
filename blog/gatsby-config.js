27a227d041bf465c49d8cc6ac7f3864791d6b612
conradj <conradsjackson@gmail.com>
2019-06-18 22:14:33 +0100

    themerise

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -66,6 +66,18 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Cinzel Decorative`,
+          `Kadwa`,
+          `Merriweather\:700`,
+          `Open Sans`,
+          // `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- blog/src/utils/typography.js
+++ blog/src/utils/typography.js
@@ -1,5 +1,19 @@
 import Typography from "typography"
 import sternGroveTheme from "typography-theme-stern-grove"
+sternGroveTheme.headerFontFamily = ["Merriweather", "serif"]
+sternGroveTheme.headerColor = "hsl(0,0,0,0.8)"
+sternGroveTheme.bodyFontFamily = ["Kadwa", "serif"]
+
+sternGroveTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+  a: {
+    color: "#d81159",
+  },
+  "a:hover, a:active": {
+    color: "#d81159",
+    transition: "all .25s ease-in-out",
+    borderBottom: "3px solid #d81159",
+  },
+})
 
 const typography = new Typography(sternGroveTheme)
 
