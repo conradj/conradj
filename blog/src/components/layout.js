f59c2d9956b18d8cae88ee6b702dd362f85c683b
conradj <conradsjackson@gmail.com>
2019-06-18 22:28:30 +0100

    add header and footer

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -34,12 +35,8 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
+        <Footer />
       </>
     )}
   />
