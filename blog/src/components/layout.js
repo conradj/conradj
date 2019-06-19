5bf42eccc8eeedc54934cc1b10697c12c0110c59
conradj <conradsjackson@gmail.com>
2019-06-19 22:07:40 +0100

    blog title in head

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -7,6 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -25,6 +26,9 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Helmet>
+          <title>{data.site.siteMetadata.title}</title>
+        </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
