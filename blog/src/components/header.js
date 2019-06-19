371b3c973707038ea77d5ec9fc6336f82236bcba
conradj <conradsjackson@gmail.com>
2019-06-19 22:07:48 +0100

    avatar in header

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -2,13 +2,19 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
+import Avatar from "./avatar"
 
 const StyledHeader = styled.header`
   border-top: solid 4px #d81159;
   border-bottom: solid 1px #78fecf;
-  padding: 0.45rem 1rem;
+`
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 1rem auto;
   display: flex;
   font-size: 1.5rem;
+  padding: "1.45rem 1.0875rem";
 `
 
 const StyledEnd = styled.div`
@@ -17,23 +23,29 @@ const StyledEnd = styled.div`
 
 const StyledMiddle = styled.div`
   flex: 1;
-  text-align: center;
+  text-align: right;
   margin-bottom: 0;
+  align-self: flex-end;
 `
 
 const StyledH1 = styled.h1`
+  font-family: monospace;
+  font-size: 0.8rem;
   margin: 0rem;
 `
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <StyledEnd>
-      <Link to="/">index</Link>
-    </StyledEnd>
-    <StyledMiddle>
-      <StyledH1>Conrad is null or not an object</StyledH1>
-    </StyledMiddle>
-    <StyledEnd></StyledEnd>
+    <Container>
+      <StyledEnd>
+        <Link to="/">
+          <Avatar />
+        </Link>
+      </StyledEnd>
+      <StyledMiddle>
+        <StyledH1>conrad is null or not an object</StyledH1>
+      </StyledMiddle>
+    </Container>
   </StyledHeader>
 )
 
