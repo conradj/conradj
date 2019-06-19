676aa6a3c753423889d17e2a75f4047ff7901762
conradj <conradsjackson@gmail.com>
2019-06-19 22:07:03 +0100

    new title

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Conrad Jackson Blog`,
-    description: `This is my blog`,
+    title: `conrad is null or not an object - A Blog`,
+    description: `blog for conrad jackson conradj`,
     author: `@conradj`,
   },
   plugins: [
@@ -57,7 +57,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/me.png`, // This path is relative to the root of the site.
       },
     },
     {
