dcacbd369b013b26df645b21449831e30f795e26
conradj <conradsjackson@gmail.com>
2019-06-21 07:19:57 +0100

    add social card component

---
+++ blog/src/components/socialcard.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+const SocialCard = ({ url, title, description, imageUrl, imageAlt }) => (
+  <Helmet>
+    <meta name="og:type" content="article" />
+    <meta name="og:headline" content={title} />
+    <meta name="og:headline" content={title} />
+    <meta name="og:url" content={url} />
+    <meta name="og:title" content={title} />
+    <meta name="og:description" content={description} />
+    <meta name="og:image" content={imageUrl} />
+    <meta name="og:image:alt" content={imageAlt} />
+    <meta name="og:image:width" content="1000" />
+    <meta name="og:image:height" content="500" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content={url} />
+    <meta property="og:headline" content={title} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={imageUrl} />
+    <meta property="og:image:alt" content={imageAlt} />
+    <meta property="og:image:width" content="1000" />
+    <meta property="og:image:height" content="500" />
+    <meta itemprop="og:headline" content={title} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@conradj" />
+    <meta name="twitter:image:alt" content={imageAlt} />
+  </Helmet>
+)
+
+export default SocialCard
