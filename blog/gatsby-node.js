ecc4d6d8526ec0bc5c52a5101bd6140422ae28c8
conradj <conradsjackson@gmail.com>
2019-06-24 22:03:49 +0100

    remove pointless code

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,5 +1,3 @@
-const fs = require("fs")
-const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
 
 // Here we're adding extra stuff to the "node" (like the slug)
@@ -15,30 +13,6 @@ exports.onCreateNode = async ({ node, actions, getNode }) => {
       name: "slug",
       // Generated value based on filepath with "blog" prefix
       value: `/${value}`,
-    })
-    const { dir } = path.parse(node.fileAbsolutePath)
-
-    const heroImage = await new Promise((res, rej) => {
-      // get a list of files in `dir`
-      fs.readdir(dir, (err, files) => {
-        if (err) rej(err)
-
-        // if there's a file named `hero`, return it
-        res(files.find(file => file.includes("hero")))
-      })
-    })
-
-    // path.relative will return a (surprise!) a relative path from arg 1 to arg 2.
-    // you can use this to set up your default hero
-    const heroPath = heroImage
-      ? `./${heroImage}`
-      : path.relative(dir, "src/images/default-hero.jpg")
-
-    // create a node with relative path
-    createNodeField({
-      node,
-      name: "hero",
-      value: `./${heroImage}`,
     })
   }
 }
